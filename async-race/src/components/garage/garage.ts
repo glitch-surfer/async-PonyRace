6eeac439256e081a4124a973e377e7e979cb62c0
Glitch-surfer <marker661@gmail.com>
2023-07-08 14:01:17 +0300

    feat: implement createCar

--- async-race/src/components/garage/garage.ts
+++ async-race/src/components/garage/garage.ts
@@ -1,5 +1,7 @@
+import type { ICarResponse } from '../../types/types';
 import { BaseComponent } from '../../utils/base-component';
 import { Controls } from './controls-component/controls';
+import { Car } from './track-component/car/car';
 import { Track } from './track-component/track';
 import type { IGarage, IGarageParams } from './types/garage-types';
 
@@ -15,5 +17,34 @@ export class Garage extends BaseComponent implements IGarage {
     this.track = new Track(params.track);
 
     this.getElement().append(this.controls.getElement(), this.track.getElement());
+    this.addCreateCarHandler();
+  }
+
+  private addCreateCarHandler(): void {
+    const createCarHandler = (): void => {
+      if (!(this.controls.createCarInput instanceof HTMLInputElement)) throw new Error('not input');
+      const newCarName = this.controls.createCarInput.value;
+      const newCarId = Math.max(...this.track.carsList.map((car) => car.id)) + 1;
+      const newCarColor = '#fff';
+      const newCarParams: ICarResponse = {
+        id: newCarId,
+        name: newCarName,
+        color: newCarColor,
+      };
+
+      const newCar = new Car(newCarParams);
+      this.track.trackList.append(newCar.getElement());
+      fetch('http://127.0.0.1:3000/garage', {
+        method: 'POST',
+        body: JSON.stringify(newCarParams),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).catch((error) => {
+        Error(error.message);
+      });
+    };
+
+    this.controls.createCarBtn.addEventListener('click', createCarHandler);
   }
 }
